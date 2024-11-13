9e157005d50099d0adf086ba2974f080f255cc92
fcbento <felipe.16costa@gmail.com>
2024-11-12 22:24:09 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,24 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angular-17-18-features';
+  userList: Array<{name: string; age: number; profession: string; id:string}> = [
+    {
+      age: 21,
+      id: '1',
+      name: 'Felipe',
+      profession: 'Software developer'
+    },
+    {
+      age: 20,
+      id: '2',
+      name: 'Joaquim',
+      profession: 'Scrum master'
+    },
+    {
+      age: 23,
+      id: '3',
+      name: 'Otavio',
+      profession: 'CEO'
+    }
+  ]
 }
