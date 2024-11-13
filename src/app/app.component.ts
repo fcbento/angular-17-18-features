8e19a7651c0fb86417bd1c1ec503bf2f35a1e3f3
fcbento <felipe.16costa@gmail.com>
2024-11-12 22:49:44 -0300

    deferrable views

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,29 +5,53 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  //templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  template: `
+    <div style="display: flex; justify-content: center; align-items: center; flex-direction: column; gap: 1rem;">
+      <button (click)="renderBlock = true">Renderizar título</button>
+
+      @defer(when renderBlock) {
+        <h3 style="color: red;">Element rendered on interaction</h3>
+      }
+      @placeholder {
+        <span>Initial placeholder content</span>
+      }
+      @loading {
+        <span>Content being loaded</span>
+      }
+      @error {
+        <span>Content being loaded</span>
+      }
+    </div>
+  `,
 })
 export class AppComponent {
   title = 'angular-17-18-features';
-  userList: Array<{name: string; age: number; profession: string; id:string}> = [
+  renderBlock = false;
+  userList: Array<{
+    name: string;
+    age: number;
+    profession: string;
+    id: string;
+  }> = [
     {
       age: 21,
       id: '1',
       name: 'Felipe',
-      profession: 'Software developer'
+      profession: 'Software developer',
     },
     {
       age: 20,
       id: '2',
       name: 'Joaquim',
-      profession: 'Scrum master'
+      profession: 'Scrum master',
     },
     {
       age: 23,
       id: '3',
       name: 'Otavio',
-      profession: 'CEO'
-    }
-  ]
+      profession: 'CEO',
+    },
+  ];
 }
